560790bdfffc240ce98e1203c61c706d1c8a77ff
ran decool <ran.decool@gmail.com>
2024-11-18 17:24:01 +0100

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,16 @@
 #   end
 
 # user
-# toto = User.create!(email: "toto@toto.com", encrypted_password: "toto")
-# manu = User.create!(email: "manu@manu.com", encrypted_password: "manu")
+toto = User.create!(email: "toto@toto.com", password: "password")
+manu = User.create!(email: "manu@manu.com", password: "password")
 
 # art
 
-# art_fleur = Art.create!(name: "peinture fleur", description: "fleurs toile gouache 40X30", price: 50, owner: toto)
-# art_statue = Art.create!(name: "statue fleur", description: "statue argile", price: 70, owner: toto)
-# art_paysage = Art.create!(name: "peinture paysage", description: "paysage toile huile 40X30", price: 100, owner: toto)
+art_fleur = Art.create!(name: "peinture fleur", description: "fleurs toile gouache 40X30", price: 50, owner: toto)
+art_statue = Art.create!(name: "statue fleur", description: "statue argile", price: 70, owner: toto)
+art_paysage = Art.create!(name: "peinture paysage", description: "paysage toile huile 40X30", price: 100, owner: toto)
 
-# # booking
+# booking
 
-# Booking.create!(art: art_fleur, user: manu, start_date: Date.today, end_date: Date.today + 2, status: "in progress" )
-# Booking.create!(art: art_paysage, user: manu, start_date: Date.today, end_date: Date.today + 2, status: "confirmed" )
+Booking.create!(art: art_fleur, user: manu, start_date: Date.today, end_date: Date.today + 2, status: "in progress" )
+Booking.create!(art: art_paysage, user: manu, start_date: Date.today, end_date: Date.today + 2, status: "confirmed" )
