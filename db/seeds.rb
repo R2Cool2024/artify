e64b329bbf3a587d7f48bb2e6b55ccf9d51fdccc
ran decool <ran.decool@gmail.com>
2024-11-18 16:55:09 +0100

    seed in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# user
+# toto = User.create!(email: "toto@toto.com", encrypted_password: "toto")
+# manu = User.create!(email: "manu@manu.com", encrypted_password: "manu")
+
+# art
+
+# art_fleur = Art.create!(name: "peinture fleur", description: "fleurs toile gouache 40X30", price: 50, owner: toto)
+# art_statue = Art.create!(name: "statue fleur", description: "statue argile", price: 70, owner: toto)
+# art_paysage = Art.create!(name: "peinture paysage", description: "paysage toile huile 40X30", price: 100, owner: toto)
+
+# # booking
+
+# Booking.create!(art: art_fleur, user: manu, start_date: Date.today, end_date: Date.today + 2, status: "in progress" )
+# Booking.create!(art: art_paysage, user: manu, start_date: Date.today, end_date: Date.today + 2, status: "confirmed" )
