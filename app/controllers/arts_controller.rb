79b1ac7a3e5ce21d8e2147eea16f338a0b1fbece
Lucille Gris <lucille.gris@gmail.com>
2024-11-20 14:15:16 +0100

    ajout du formulaire permettant d'ajouter une oeuvre

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -6,4 +6,37 @@ class ArtsController < ApplicationController
   def show
     @art = Art.find(params[:id])
   end
+
+  def new
+    @art = Art.new
+    @artists_name = []
+    Artist.all.each do |artist|
+      @artists_name << artist.name
+    end
+  end
+
+  def create
+    @artist = Artist.find(art_params["artist"])
+    @art = Art.new
+    @art.artist = @artist
+    @art.name = art_params["name"]
+    @art.description = art_params["description"]
+    @art.category = art_params["category"]
+    @art.year = art_params["year"].to_i
+    @art.price = art_params["price"].to_i
+    @art.photo = art_params["photo"]
+    @art.owner = User.last
+    if @art.save!
+      redirect_to art_path(@art)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def art_params
+    params.require(:art).permit(:name, :category, :year, :price, :description, :artist, :photo)
+  end
+
 end
