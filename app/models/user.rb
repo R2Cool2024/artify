1b5f7892cebd02db3f6711319313c11aacf9e208
Lucille Gris <lucille.gris@gmail.com>
2024-11-21 10:39:27 +0100

    update migration (ajout name to users)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
-class User < ApplicationRecord  
+class User < ApplicationRecord
   has_many :bookings
   has_many :arts, through: :bookings, foreign_key: :owner_id
 
   has_one_attached :photo
 
-  
+  validates :name, presence: true, length: { in: 2..30 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
